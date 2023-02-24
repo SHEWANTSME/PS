aa5919947f62974dcd7bd63926028d7b598acbed
SHEWANTSME <qhdmswns@mju.ac.kr>
2023-02-25 01:28:45 +0900

    이번엔 set으로 해봄 -> 훨씬 빠름

--- BOJ/Silver/2210.cpp
+++ BOJ/Silver/2210.cpp
@@ -6,6 +6,7 @@
 #include<vector>
 #include<algorithm>
 #include<string>
+#include<set>
 //#include<string.h> // for using memset(0 or -1)로 초기화 할때 사용 
 // 지금은 visited를 초기화 할 필요가 없으니 사용 x
 using namespace std;
@@ -14,15 +15,20 @@ int dy[4] = {0,1,-1,0};
 int arr[5][5];
 string temp="";
 vector<string>v; // 값어치 있는 temp만 담는 vector
+set<string>st;
 //backtracking 내 방식대로 짜보자
 void DFS(int x, int y){ // void 형태의 backtracking 외에도 다른 자료형 형태의 backtracking도 생각해보자
     if(temp.size()==6){ // 다른 사람들은 그냥 set을 사용하더라 -> set이 중복허용이 안되니까 그냥 set 사용한듯
-        bool flag=0;
-        for(auto &e : v){
-            if(e==temp)flag=1;
-        }
-        if(flag==1){ return;}
-        else{v.push_back(temp);return;} 
+        // bool flag=0;
+        // for(auto &e : v){
+        //     if(e==temp)flag=1;
+        // }
+        // if(flag==1){ return;}
+        // else{v.push_back(temp);return;} 
+
+        // 이번엔 set 을 사용해서 해보자
+        st.insert(temp);
+        return;
     }
     for(int i=0 ; i<4 ; i++){
         int nx = dx[i]+x;
@@ -44,5 +50,9 @@ int main(){
             temp="";
         }
     }
-    cout<<v.size()<<endl;
+    //cout<<v.size()<<endl;
+    // for(auto &e : st){
+    //     cout<<e<<endl;
+    // }
+    cout<<st.size()<<endl;
 }
