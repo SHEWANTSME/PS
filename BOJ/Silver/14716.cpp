82878f8121f2f2684c7c60b1720f9f16e34d15ea
SHEWANTSME <qhdmswns@mju.ac.kr>
2023-03-05 01:54:17 +0900

    이제는 맞는 풀이. 뭉텅이를 찾는 방법은 유용하니 잘 알아둘것. 나의 경우에는 DFS가 더 편함

--- BOJ/Silver/14716.cpp
+++ BOJ/Silver/14716.cpp
@@ -13,15 +13,19 @@ bool visited[251][251];
 int cnt=0;
 void DFS(int x, int y){ // 처음엔 visited만 생각해줬는데 
 // 해당 문제에서는 방문여부와 현재 arr의 값이 1인 뭉텅이만 찾고 싶으니까
-    if(visited[x][y])return;// 여기에도 추가적인 조건이 들어가야하고
+    if(visited[x][y] or arr[x][y] == 0)return;// 이렇게 바뀌고
     visited[x][y]=1;
     for(int i=0 ; i<8 ; i++){
         int nx = x + dx[i];
         int ny = y + dy[i];
         if(nx<0 or ny<0 or nx>=r or ny >=c)continue;
-        if(visited[nx][ny])continue;//여기에도 들어가야겠죠?
+        if(visited[nx][ny] or arr[nx][ny]==0)continue;//이렇게 바뀌어야겠죠
         DFS(nx,ny);
     }
+    // 뭉텅이를 찾는 노하우는 알아두는게 좋음.
+    // 그리고 왜 맨첨 if랑 for문 안의 if문이 겹치냐고 생각할 수도 있는데
+    // main에서 DFS에 맨 처음 들어갈때 맨처음 요소는 for문으로 안걸러지니까 
+    // 저렇게 맨첨if랑 for안의 if랑 써줘야하는거임.
 }
 int main(){
     cin>>r>>c;
@@ -38,4 +42,4 @@ int main(){
     }
     cout<<cnt<<endl;
 }
-// 위 풀이는 틀렸다. 왜 틀렸을까? 생각해보면 
+// 이젠 맞는 풀이
