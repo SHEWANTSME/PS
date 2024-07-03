34f347d38fbe2f841cc8d67c0c4a82f8b5a8c5ae
SHEWANTSME <qhdmswns@mju.ac.kr>
2024-07-03 10:32:37 +0900

    OX문제 이전 틀린코드 주석처리해두고 추가해둠

--- BOJ/Silver/27970.cpp
+++ BOJ/Silver/27970.cpp
@@ -26,3 +26,31 @@ int main(){
     }
     cout<<ans<<endl;
 }
+/*
+기존코드
+#include<iostream>
+#include<algorithm>
+#include<string>
+#define ll unsigned long long
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    ll tmp = 1;
+    ll ans=0;
+    for(int i=0;i<s.size();i++){
+        tmp%=1000000007;
+        tmp*=2;
+        tmp%=1000000007;
+        if(s[i]=='X'){continue;}
+        else{
+            if(i==0)tmp/=2;
+            ans%=1000000007;
+            ans+=tmp;
+            ans%=1000000007;
+        }
+    }
+    cout<<ans<<endl;
+}
+-> XOX같은 경우를 처리를 제대로 하지 못함
+*/
