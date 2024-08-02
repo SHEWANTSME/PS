a2051b32086985b9e616fc960b188357ed03e180
SHEWANTSME <qhdmswns@mju.ac.kr>
2024-08-02 15:57:06 +0900

    5052번 - 전화번호 목록 - 트라이로 구현했는데 한번에 떠올리기가 어려운듯- 주석 추가

--- BOJ/Gold/5052.cpp
+++ BOJ/Gold/5052.cpp
@@ -2,40 +2,40 @@
 
 // 트라이로 풀 수 있다고 하는데 트라이 아직 모르니까 걍 정렬하고 해보자
 
-// //1. 그냥 푸는 방법
-// #include <iostream>
-// #include <vector>
-// #include <string>
-// #include <algorithm>
-// using namespace std;
-// vector<string>phone_num;
+//1. 그냥 푸는 방법
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+vector<string>phone_num;
 
-// int main() {
-//     int t; cin >> t;
-//     while (t--) {
-//         int n; cin >> n;
-//         for(int i=0;i<n;i++){
-//             string s; cin>>s;
-//             phone_num.push_back(s);
-//         }
-//         sort(phone_num.begin(), phone_num.end());
-//         // for(auto &e : phone_num){
-//         //     cout<<e<<endl;
-//         // }
-//         bool flag = 1;
-//         for (int i = 0; i < n - 1; ++i) {
-//             if (phone_num[i].size() <= phone_num[i + 1].size() and phone_num[i] == phone_num[i + 1].substr(0, phone_num[i].size())) {
-//                 //substr(a,b) -> a 인덱스부터 b길이만큼 문자열을 반환
-//                 // substr(a)-> a 인덱스부터 끝까지 문자열을 반환
-//                 cout << "NO" << endl;
-//                 flag = false;
-//                 break;
-//             }
-//         }
-//         if (flag) cout << "YES" << endl;
-//         phone_num.clear();
-//     }
-// }
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        for(int i=0;i<n;i++){
+            string s; cin>>s;
+            phone_num.push_back(s);
+        }
+        sort(phone_num.begin(), phone_num.end());
+        // for(auto &e : phone_num){
+        //     cout<<e<<endl;
+        // }
+        bool flag = 1;
+        for (int i = 0; i < n - 1; ++i) {
+            if (phone_num[i].size() <= phone_num[i + 1].size() and phone_num[i] == phone_num[i + 1].substr(0, phone_num[i].size())) {
+                //substr(a,b) -> a 인덱스부터 b길이만큼 문자열을 반환
+                // substr(a)-> a 인덱스부터 끝까지 문자열을 반환
+                cout << "NO" << endl;
+                flag = false;
+                break;
+            }
+        }
+        if (flag) cout << "YES" << endl;
+        phone_num.clear();
+    }
+}
 
 // 2. 트라이로 푸는 방법
 // 그림 그려가면서 생각해보면 그나마 쉬움
