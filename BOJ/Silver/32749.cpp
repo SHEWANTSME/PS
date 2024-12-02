32eeba7e946b165ef860630d83d79b8d53df9102
SHEWANTSME <qhdmswns@mju.ac.kr>
2024-12-02 17:21:20 +0900

    처음엔 substr을 반복해서 recursive하게 했는데 자꾸 틀려서 slicing했음

---
+++ BOJ/Silver/32749.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int n,t;
+string s;
+int main() {
+    cin>>n>>t>>s;
+    int power=(1<<n);//2^n
+    int digits=(1 << (n-t));//2^(n-t)
+    string ans= "";
+
+    for (int i=0; i<power; i+=digits) {//slicing
+        string cur = s.substr(i, digits);
+        ans=max(ans, cur);
+    }
+
+    cout<<ans<<endl;
+}
