33ab224e1f1ec1b7a0efde194f8de14c3e75470e
SHEWANTSME <qhdmswns@mju.ac.kr>
2025-01-22 15:29:41 +0900

    미세먼지 안녕 - Done solving - 구현문제 -  공기순환 짜는게 빡셈

--- BOJ/Gold/17144.cpp
+++ BOJ/Gold/17144.cpp
@@ -2,65 +2,47 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<string.h>
 using namespace std;
 int R,C,T;
 int arr[51][51];
 int new_arr[51][51];
+int tmp[51][51];
 int dx[4] = {0,-1,0,1};
 int dy[4] = {1,0,-1,0};
 // 1. Diffuse -> 2. Move
 int air_x,ans;
-int Activate_1(int x, int y){
-    // 1. -> , up , <- , down  (0,1) , (-1,0) , (0,-1), (1,0)
-    int cnt=0;
-    int tmp=0;
-    for(int i= air_x-2; i>=1; i--){
-        cnt++;
-        tmp+=new_arr[i][1];
-        if(cnt==1)return tmp;
+void Move_1(int x,int y){
+    for(int i=2; i<=C-1;i++){
+        tmp[air_x-1][i+1] = new_arr[air_x-1][i];
     }
-    for(int i=2; i<=C;i++){
-        cnt++;
-        tmp+=new_arr[1][i];
-        if(cnt==1)return tmp;
+    for(int i=air_x-1; i>=2; i--){
+        tmp[i-1][C] = new_arr[i][C];
     }
-    for(int i=2;i<=air_x-1;i++){
-        cnt++;
-        tmp+=new_arr[i][C];
-        if(cnt==1)return tmp;
+    for(int i= C;i>=2; i--){
+        tmp[1][i-1]=new_arr[1][i];
     }
-    for(int i=C-1; i>=2;i--){
-        cnt++;
-        tmp+=new_arr[air_x-1][i];
-        if(cnt==1)return tmp;
+    for(int i=1;i<=air_x-2;i++){
+        tmp[i+1][1]= new_arr[i][1];
     }
-    return tmp;
+    if(tmp[air_x-1][1]>0)tmp[air_x-1][1]=0;
 }
-int Activate_2(int x, int y){
-    int cnt=0;
-    int tmp=0;
-    for(int i= air_x+1; i<=R; i++){
-        cnt++;
-        tmp+=new_arr[i][1];
-        if(cnt==1)return tmp;
+void Move_2(int x, int y){
+    for(int i=2; i<=C-1;i++){
+        tmp[air_x][i+1] = new_arr[air_x][i];
     }
-    for(int i=2; i<=C;i++){
-        cnt++;
-        tmp+=new_arr[R][i];
-        if(cnt==1)return tmp;
+    for(int i=air_x; i<=R-1;i++){
+        tmp[i+1][C] = new_arr[i][C];
     }
-    for(int i=R-1;i>=air_x;i++){
-        cnt++;
-        tmp+=new_arr[i][C];
-        if(cnt==1)return tmp;
+    for(int i=C; i>=2;i--){
+        tmp[R][i-1] = new_arr[R][i];
     }
-    for(int i=C-1; i>=2;i--){
-        cnt++;
-        tmp+=new_arr[air_x][i];
-        if(cnt==1)return tmp;
+    for(int i=R; i>=air_x+1;i--){
+        tmp[i-1][1] = new_arr[i][1];
     }
-    return tmp;
+    if(tmp[air_x][1]>0)tmp[air_x][1]=0;
 }
+
 void Diffuse(int x, int y){
     int now = arr[x][y];
     vector<int>tmp;
@@ -90,62 +72,55 @@ int main(){
     }
     // 공기청정기의 좌표는 -> (airx, 1) , (airx-1 ,1)가 되겠군
 
-    // 1. Diffuse
-
     for(int k=1 ; k<=T ; k++){
+        // 1. Diffuse
+        memset(new_arr, 0, sizeof(new_arr));
+        memset(tmp, 0, sizeof(tmp));
         for(int i=1; i<=R;i++){
             for(int j=1; j<=C;j++){
                 if(arr[i][j]==-1 or arr[i][j]==0)continue;
                 Diffuse(i,j);
             }
         }
-        // for(int i=1; i<=R;i++)
-        //     for(int j=1; j<=C;j++)
-        //         ans+=new_arr[i][j];
+
         new_arr[air_x][1]=-1;
         new_arr[air_x-1][1]=-1;
 
-        for(int i=2 ; i<air_x-1; i++)
-            for(int j=2 ; j<=C-1; j++)
-                ans+=new_arr[i][j];
-        for(int i= air_x+1;i<=R-1;i++)
-            for(int j=2; j<=C-1;j++)
-                ans+=new_arr[i][j];
+        //Move1
+        Move_1(air_x-1,1);
+        //Move2
+        Move_2(air_x,1);
 
-        int q = Activate_1(air_x-1,1);
-        int w = Activate_2(air_x,1);
-        ans -=(q+w);
+        for(int i=1;i<=R;i++){
+            new_arr[i][1] = tmp[i][1];
+            new_arr[i][C]=tmp[i][C];
+        }
+        for(int i=1; i<=C;i++){
+            new_arr[1][i] = tmp[1][i];
+            new_arr[R][i] = tmp[R][i];
+            new_arr[air_x-1][i]=tmp[air_x-1][i];
+            new_arr[air_x][i]=tmp[air_x][i];
+        }
+        new_arr[air_x][1]=-1;
+        new_arr[air_x-1][1]=-1;
+
+
+
+        // arr = new_arr 해주고 loop 돌기
         for(int i=1; i<=R;i++){
             for(int j=1; j<=C;j++){
                 arr[i][j] = new_arr[i][j];
             }
         }
-        // cout<<aans<<endl;
-        // cout<<Activate_1(air_x-1,1)<<endl;
-        // cout<<Activate_2(air_x,1)<<endl;
+
+
+
     }
-    cout<<ans<<endl;
 
-
-    // for(int i=1; i<=R;i++){
-    //     for(int j=1; j<=C;j++){
-    //         cout<<new_arr[i][j]<<" ";
-    //     }cout<<endl;
-    // }
-    // 2. purifier activate -> T초동안
-
-    // ans = aans - (Activate_1(air_x-1,1) + Activate_2(air_x,1));
-    // cout<<ans<<endl;
-
-
-    // for(int i=2 ; i<air_x-1; i++)
-    //     for(j=2 ; j<=C-1; j++)
-    //         ans+=new_arr[i][j];
-    // for(int i= air_x+1;i<=R-1;i++)
-    //     for(int j=2; j<=C-1;j++)
-    //         ans+=new_arr[i][j];
-    
-    //Activate_1(arix-1,1);// 위쪽 cycle
-    //Activate_2(airx,1);// 아래쪽 cycle
-
+    for(int i=1; i<=R;i++){
+        for(int j=1; j<=C;j++){
+            ans +=new_arr[i][j];
+        }
+    }
+    cout<<ans+2<<endl;
 }
