338ab1191cc17f0c53aa0a3ff31ac55fb31488d3
SHEWANTSME <qhdmswns@mju.ac.kr>
2025-01-24 15:14:36 +0900

    단축키 지정 - S1 - 구현 귀찮.. answer code

--- BOJ/Silver/1283.cpp
+++ BOJ/Silver/1283.cpp
@@ -1,99 +1,101 @@
-//단축키 지정 - S1- 2025/01/24
-
 #include<iostream>
 #include<algorithm>
 #include<string>
+#include<vector>
+#define endl "\n"
 using namespace std; 
+
 int n; 
 int Alpha[26];
-int chk[6];
-vector<char>ans;
-int main(){
-    cin>>n;
-    cin.ignore();// 버퍼비우기 -> 이거 안하면 개행문자가 들어가서 입력이 제대로 안됨
-    int cnt =0;
-    while(n--){
+vector<pair<char, int>> ans;
+
+int main() {
+    cin >> n;
+    cin.ignore();// 버퍼비우기
+    int cnt = 0;
+    while (n--) {
         string s;
-        getline(cin,s);// 띄어쓰기 포함해서 입력받기 -> 여기서는 ignore 안해도됨 -> getline은 버퍼를 비워줌
+        getline(cin, s);// 공백포함해서 입력받기 
         string tmp = "";
-        vector<string>v;
-        //cout<<"뀨?  : "<<s<<endl;
-        for(int i=0; i<s.size();i++){
-            if(s[i]==' '){
+        vector<string> v;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ' ') {
                 v.push_back(tmp);
-                tmp="";
-            }
-            else{
+                tmp = "";
+            } else {
                 tmp.push_back(s[i]);
             }
         }
-        v.push_back(tmp);
+        v.push_back(tmp); // v에 공백을 기준으로 나눠서 넣어줌
 
-        // for(auto &e : v){
-        //     cout<<"fslkjsdf"<<e<<endl;
-        //     cout<<e[0]<<endl;
-        // }
-
-        bool flag= 0;
-        for(int i=0; i<v.size();i++){
-            if(v[i][0]>='a' and v[i][0]<='z'){
-                if(Alpha[v[i][0]-'a']!=1){Alpha[v[i][0]-'a']=1;flag=1;ans.push_back(v[i][0]);break;}
-            }
-            if(v[i][0]>='A' and v[i][0]<='Z'){
-                if(Alpha[v[i][0]-'A']!=1){Alpha[v[i][0]-'A']=1;flag=1;ans.push_back(v[i][0]);break;}
-            }
-        }
-        if(flag==0){
-            bool fflag=0;
-            for(int i=0; i<v.size();i++){
-                for(auto &e : v[i]){
-                    if(e>='a' and e<='z'){
-                        if(Alpha[e-'a']==1)continue;
-                        else {Alpha[e-'a']=1;fflag=1;ans.push_back(e);break;}
-                    }
-                    if(e>='A' and e<='Z'){
-                        if(Alpha[e-'A']==1)continue;
-                        else {Alpha[e-'A']=1;fflag=1;ans.push_back(e);break;}
-                    }
+        bool flag = 0;
+        for (int i = 0; i < v.size(); i++) {
+            if (v[i][0] >= 'a' && v[i][0] <= 'z') {
+                if (Alpha[v[i][0] - 'a'] != 1) {
+                    Alpha[v[i][0] - 'a'] = 1;
+                    flag = 1;
+                    int pos = s.find(v[i][0], s.find(v[i]));// find함수는 찾는 문자가 없으면 npos를 반환
+                    // s.find( 찾을 문자열 , 시작 위치 , 찾을 문자열의 길이 )
+                    // v[i]의 첫번째 문자열을 찾고, 그 문자열이 나온 위치를 반환
+                    ans.push_back({v[i][0], pos});
+                    break;
                 }
             }
-            if(fflag==0){
-                ans.push_back('-');
+            if (v[i][0] >= 'A' && v[i][0] <= 'Z') {
+                if (Alpha[v[i][0] - 'A'] != 1) {
+                    Alpha[v[i][0] - 'A'] = 1;
+                    flag = 1;
+                    int pos = s.find(v[i][0], s.find(v[i]));
+                    ans.push_back({v[i][0], pos});
+                    break;
+                }
             }
         }
 
-        // string realans="";
-        // bool flagg=0;
-        // for(int i=0; i<s.size();i++){
-        //     if(s[i] == ans[cnt]){
-        //         realans.push_back('[');
-        //         realans.push_back(s[i]);
-        //         realans.push_back(']');
-        //         flagg=1;
-        //     }
-        //     else realans.push_back(s[i]);
-        // }
-        // cout<<realans<<endl;
-        // cnt++;
-        string realans="";
-        bool flagg=0;
-        for(int i=0; i<v.size();i++){
-            for(int j=0; j<v[i].size(); j++){
-                //cout<<v[i][j]<< " "<<ans[cnt]<<" " <<flagg<<" "<<chk[i]<<endl;
-                if(v[i][j]==ans[cnt] and flagg==0 and chk[i]==0){
-                    realans.push_back('[');
-                    realans.push_back(v[i][j]);
-                    realans.push_back(']');
-                    flagg=1;chk[i]=1;
+        if (not flag) {
+            bool fflag = 0;
+            for (int i = 0; i < v.size(); i++) {
+                for (int j = 0; j < v[i].size(); j++) {
+                    char e = v[i][j];
+                    if (e >= 'a' && e <= 'z') {
+                        if (Alpha[e - 'a'] == 1) continue;
+                        else {
+                            Alpha[e - 'a'] = 1;
+                            fflag = 1;
+                            ans.push_back({e, s.find(e, s.find(v[i]))});
+                            break;
+                        }
+                    }
+                    if (e >= 'A' && e <= 'Z') {
+                        if (Alpha[e - 'A'] == 1) continue;
+                        else {
+                            Alpha[e - 'A'] = 1;
+                            fflag = 1;
+                            ans.push_back({e, s.find(e, s.find(v[i]))});
+                            break;
+                        }
+                    }
                 }
-                else realans.push_back(v[i][j]);
+                if (fflag) break;
             }
-            realans.push_back(' ');
+
+            if (not fflag) {
+                ans.push_back({'-', -1});
+            }
         }
-        cout<<realans<<endl;cnt++;
+
+        string realans = "";
+        for (int i = 0; i < s.size(); i++) {
+            if (i == ans[cnt].second) {
+                realans.push_back('[');
+                realans.push_back(s[i]);
+                realans.push_back(']');
+            } else {
+                realans.push_back(s[i]);
+            }
+        }
+        cout << realans << endl;
+        cnt++;
     }
-    // for(auto &e : ans){
-    //     cout<<e<<endl;
-    // }
-
 }
