0edf0e620cee3511839b5e75d8203749062da55e
SHEWANTSME <qhdmswns@mju.ac.kr>
2025-07-26 16:32:20 +0900

    돌아가는 route도 있는걸 생각해줘야함

---
+++ BOJ/Silver/34043.cpp
@@ -0,0 +1,9 @@
+// 수라도 - S1 - 2025.07.14
+// 돌아가는 루트도 있는거 생각해줘야함!!
+#include<iostream>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    if(n==1 or n==3) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+}
