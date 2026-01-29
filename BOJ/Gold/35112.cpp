b151beb0918e6d6679eb5ec1c33f0d64b0d8f951
SHEWANTSME <qhdmswns@mju.ac.kr>
2026-01-29 17:18:25 +0900

    으악그래프 -> adhoc -> 조건잘읽고 처음 n,m만으로 판단하면 됨

---
+++ BOJ/Gold/35112.cpp
@@ -0,0 +1,15 @@
+// 으악그래프-G5-2026.01.29
+// adhoc - 조건 잘 읽고 처음 n,m만으로 판단하면 됨
+#include <iostream>
+using namespace std;
+int main() {
+    int n,m;
+    cin>>n>>m;
+    int M=m;
+    while(M--){
+        int u,v;
+        cin>>u>>v;
+    }
+    if(m<=n)cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
